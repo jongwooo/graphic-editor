c8e4d8b22be63df8bc9bc5958f40646d06383d00
jongwooo <han980817@gmail.com>
2022-03-15 03:10:09 +0900

    feat: Use radio button instead of button

--- src/tool/ToolBar.java
+++ src/tool/ToolBar.java
@@ -1,30 +1,40 @@
 package tool;
 
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
 import javax.swing.JToolBar;
 
 public class ToolBar extends JToolBar {
     private static final long serialVersionUID = 1L;
 
-    private JButton drawRectangleBtn;
-    private JButton drawOvalBtn;
-    private JButton drawLineBtn;
-    private JButton drawPolygonBtn;
+    private JRadioButton drawRectangleBtn;
+    private JRadioButton drawOvalBtn;
+    private JRadioButton drawLineBtn;
+    private JRadioButton drawPolygonBtn;
 
     public ToolBar(String toolBarTitle) {
         super(toolBarTitle);
+        ButtonGroup toolBtnGroup = new ButtonGroup();
 
-        this.drawRectangleBtn = new JButton(new ImageIcon("src/image/rectangle.png"));
+        this.drawRectangleBtn = new JRadioButton();
+        this.drawRectangleBtn.setIcon(new ImageIcon("src/image/rectangle.png"));
+        toolBtnGroup.add(drawRectangleBtn);
         this.add(this.drawRectangleBtn);
 
-        this.drawOvalBtn = new JButton(new ImageIcon("src/image/oval.png"));
+        this.drawOvalBtn = new JRadioButton();
+        this.drawOvalBtn.setIcon(new ImageIcon("src/image/oval.png"));
+        toolBtnGroup.add(drawOvalBtn);
         this.add(this.drawOvalBtn);
 
-        this.drawLineBtn = new JButton(new ImageIcon("src/image/line.png"));
+        this.drawLineBtn = new JRadioButton();
+        this.drawLineBtn.setIcon(new ImageIcon("src/image/line.png"));
+        toolBtnGroup.add(drawLineBtn);
         this.add(this.drawLineBtn);
 
-        this.drawPolygonBtn = new JButton(new ImageIcon("src/image/polygon.png"));
+        this.drawPolygonBtn = new JRadioButton();
+        this.drawPolygonBtn.setIcon(new ImageIcon("src/image/polygon.png"));
+        toolBtnGroup.add(drawPolygonBtn);
         this.add(this.drawPolygonBtn);
     }
 }
