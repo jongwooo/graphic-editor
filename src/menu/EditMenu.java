aa1f8245a0a476c4c918ba5215fccad91a626a8a
jongwooo <han980817@gmail.com>
2022-03-20 19:25:05 +0900

    feat: Add ActionHandler to Menu

--- src/menu/EditMenu.java
+++ src/menu/EditMenu.java
@@ -4,19 +4,70 @@ import global.menu.EditMenuItem;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
 public class EditMenu extends JMenu {
     private static final long serialVersionUID = 1L;
 
+    private ActionHandler actionHandler;
+
     public EditMenu(String menuTitle) {
         super(menuTitle);
+        actionHandler = new ActionHandler();
 
         Arrays.stream(EditMenuItem.values()).forEach(editMenuItem -> {
             JMenuItem menuItem = new JMenuItem(editMenuItem.getMenuName());
+            menuItem.setActionCommand(editMenuItem.name());
+            menuItem.addActionListener(actionHandler);
             this.add(menuItem);
 
             if (editMenuItem.separated()) this.addSeparator();
         });
+    }
+
+    public void undo() {
+
+    }
+
+    public void redo() {
+
+    }
+
+    public void cutItem() {
+
+    }
+
+    public void copyItem() {
+
+    }
+
+    public void pasteItem() {
+
+    }
+
+    public void groupItem() {
+
+    }
+
+    public void ungroupItem() {
+
+    }
+
+    public void invokeMethod(String methodName) {
+        try {
+            this.getClass().getMethod(methodName).invoke(this);
+        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private class ActionHandler implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            invokeMethod(e.getActionCommand());
+        }
     }
 }
--- src/menu/FileMenu.java
+++ src/menu/FileMenu.java
@@ -4,19 +4,70 @@ import global.menu.FileMenuItem;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
 public class FileMenu extends JMenu {
     private static final long serialVersionUID = 1L;
 
+    private ActionHandler actionHandler;
+
     public FileMenu(String menuTitle) {
         super(menuTitle);
+        actionHandler = new ActionHandler();
 
         Arrays.stream(FileMenuItem.values()).forEach(fileMenuItem -> {
             JMenuItem menuItem = new JMenuItem(fileMenuItem.getMenuName());
+            menuItem.setActionCommand(fileMenuItem.name());
+            menuItem.addActionListener(actionHandler);
             this.add(menuItem);
 
             if(fileMenuItem.separated()) this.addSeparator();
         });
+    }
+
+    public void newFile() {
+
+    }
+
+    public void openFile() {
+
+    }
+
+    public void closeEditor() {
+
+    }
+
+    public void saveFile() {
+
+    }
+
+    public void saveFileAs() {
+
+    }
+
+    public void printFile() {
+
+    }
+
+    public void quitEditor() {
+        System.exit(0);
+    }
+
+    public void invokeMethod(String methodName) {
+        try {
+            this.getClass().getMethod(methodName).invoke(this);
+        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private class ActionHandler implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            invokeMethod(e.getActionCommand());
+        }
     }
 }
