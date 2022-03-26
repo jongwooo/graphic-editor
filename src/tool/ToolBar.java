e506714d5a68fb7ce99bf769aa416b2a7ed3ccab
jongwooo <han980817@gmail.com>
2022-03-27 04:07:29 +0900

    refactor: Set default button to rectangle

--- src/tool/ToolBar.java
+++ src/tool/ToolBar.java
@@ -56,7 +56,7 @@ public class ToolBar extends JToolBar {
     }
 
     private void setDefaultBtn() {
-        JRadioButton defaultBtn = (JRadioButton) this.getComponent(ShapeToolItem.cursor.ordinal());
+        JRadioButton defaultBtn = (JRadioButton) this.getComponent(ShapeToolItem.rectangle.ordinal());
         defaultBtn.doClick();
     }
 }
