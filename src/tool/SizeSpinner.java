87048eac20a8ad379e6602dbb2e05f1996745ffa
Jongwoo Han <han980817@gmail.com>
2022-05-30 10:03:08 +0900

    style: Update SizeSpinner.java to format code

--- src/tool/SizeSpinner.java
+++ src/tool/SizeSpinner.java
@@ -8,10 +8,10 @@ public abstract class SizeSpinner extends JSpinner {
   public SizeSpinner(SpinnerModel model) {
     super(model);
 
-    if ( this.getEditor() instanceof JSpinner.DefaultEditor ) {
-      JSpinner.DefaultEditor editor = ( JSpinner.DefaultEditor ) this.getEditor();
-      editor.getTextField().setEnabled( true );
-      editor.getTextField().setEditable( false );
+    if (this.getEditor() instanceof JSpinner.DefaultEditor) {
+      JSpinner.DefaultEditor editor = (JSpinner.DefaultEditor) this.getEditor();
+      editor.getTextField().setEnabled(true);
+      editor.getTextField().setEditable(false);
     }
   }
 }
